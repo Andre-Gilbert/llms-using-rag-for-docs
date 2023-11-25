35e724488089ecb8df15ccf7e227b19817a194e7
Andre Gilbert <andre.gilbert.77110@gmail.com>
2023-11-25 14:21:48 +0100

    added example test case format

---
+++ src/test_cases.py
@@ -0,0 +1,9 @@
+"""Test cases."""
+
+TEST_CASES = [
+    {
+        "user_prompt": "Do xzy",
+        "data": "Maybe some data input such as pandas DataFrame, list, string, other...",
+        "expected_output": "Results of executed code, e.g. pandas DataFrame, list, string, other...",
+    }
+]
