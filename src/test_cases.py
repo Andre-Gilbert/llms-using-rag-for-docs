08a3105399eb7390fdb09180ad7cc551a9010016
FelixNoll <felixnoll.fn@gmail.com>
2023-12-07 18:51:30 +0100

    Minor adjustment in test cases

--- src/test_cases.py
+++ src/test_cases.py
@@ -10,13 +10,13 @@ TEST_CASES = [
     {
         "id": 1,
         "user_prompt": """This is my Dataframe:({'Name': ['Alice', 'Bob', 'Aritra'], 'Age': [25, 30, 35], 'Location': ['Seattle', 'New York', 'Kona']},index=([10, 20, 30])) Please display the df while making sure to change the index to 100, 200 and 300 """, # prompt that we send the agent
-        "data": """data = pd.DataFrame=({'Name': ['Alice', 'Bob', 'Aritra'], 'Age': [25, 30, 35], 'Location': ['Seattle', 'New York', 'Kona']},index=([10, 20, 30]))""", # the data needed should always be named 'data'
+        "data": """data = pd.DataFrame({'Name': ['Alice', 'Bob', 'Aritra'], 'Age': [25, 30, 35], 'Location': ['Seattle', 'New York', 'Kona']},index=([10, 20, 30]))""", # the data needed should always be named 'data'
         "correct_function": """import pandas as pd\ndef correct_function(data):\n    data.index = [100, 200, 300]\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 2,
         "user_prompt": """({'animal': ['alligator', 'bee', 'falcon', 'lion','monkey', 'parrot', 'shark', 'whale', 'zebra']}) This is my dataframe. Please show me the head of all columns but the last 4 """, # prompt that we send the agent
-        "data": """data = pd.DataFrame=({'animal': ['alligator', 'bee', 'falcon', 'lion','monkey', 'parrot', 'shark', 'whale', 'zebra']})""", # the data needed should always be named 'data'
+        "data": """data = pd.DataFrame({'animal': ['alligator', 'bee', 'falcon', 'lion','monkey', 'parrot', 'shark', 'whale', 'zebra']})""", # the data needed should always be named 'data'
         "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data.iloc[:, :-4].head()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
@@ -35,37 +35,37 @@ TEST_CASES = [
         "id": 5,
         "user_prompt": """data1 = [[1, 8, 2], [1, 2, 5], [2, 5, 8], [2, 6, 9]] data2 = pd.DataFrame(data, columns=["a", "b", "c"] , index=["tiger", "leopard", "cheetah", "lion"]) Given Data is my Data and df is my Dataframe. Both are part of your argument. Please groupby that dataframe by "a" and give the product as well """, # prompt that we send the agent
         "data": """data_1 = [[1, 8, 2], [1, 2, 5], [2, 5, 8], [2, 6, 9]]\ndata_2 = pd.DataFrame= (data, columns=["a", "b", "c"] , index=["tiger", "leopard", "cheetah", "lion"])""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = df.groupby('a').prod()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data_2 = data.groupby('a').prod()\n    data=data_2    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 6,
         "user_prompt": """a = pd.Series([1, 1, 1, np.nan], index=['a', 'b', 'c', 'd']) b = pd.Series([1, np.nan, 1, np.nan], index=['a', 'b', 'd', 'e'])  Please take a and b as your arguments and divide a by b. Please also use the fill value 0 """, # prompt that we send the agent
         "data": """data_1 = pd.Series([1, 1, 1, np.nan], index=['a', 'b', 'c', 'd'])\ndata_2 = pd.Series([1, np.nan, 1, np.nan], index=['a', 'b', 'd', 'e'])""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data_1.div(data_2, fill_value=0)\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\nimport numpy as np\ndef correct_function(data_1, data_2):\n    data = data_1.div(data_2, fill_value=0)\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 7,
         "user_prompt": """data = {('level_1', 'c', 'a'): [3, 7, 11],('level_1', 'd', 'b'): [4, 8, 12],('level_2', 'e', 'a'): [5, 9, None],('level_2', 'f', 'b'): [6, 10, None],}Please drop a""", # prompt that we send the agent
-        "data": """data = pd.DataFrame={('level_1', 'c', 'a'): [3, 7, 11],('level_1', 'd', 'b'): [4, 8, 12],('level_2', 'e', 'a'): [5, 9, None],('level_2', 'f', 'b'): [6, 10, None],}""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    df = pd.DataFrame(data)\n    data = df.droplevel(2, axis=1)\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "data": """data = pd.DataFrame({('level_1', 'c', 'a'): [3, 7, 11],('level_1', 'd', 'b'): [4, 8, 12],('level_2', 'e', 'a'): [5, 9, None],('level_2', 'f', 'b'): [6, 10, None],})""", # the data needed should always be named 'data'
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data.droplevel(2, axis=1)\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 8,
         "user_prompt": """Please take follwing Series and order it ascending while making sure NAN values are at the beginning s = pd.Series([np.nan, 1, 3, 10, 5, np.nan]) """, # prompt that we send the agent
         "data": """data = pd.Series([np.nan, 1, 3, 10, 5, np.nan])""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = s.sort_values(na_position='first')\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data.sort_values(na_position='first')\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 9,
         "user_prompt": """data1 = {'Name': ['Alice', 'Bob', 'Charlie'],'Age': [25, 30, 22],'City': ['New York', 'San Francisco', 'Los Angeles']}data2= {'Name': ['Alice', 'John', 'Charlie'],'Age': [25, 31, 22],'City': ['New York', 'San Francisco', 'Los Angeles']}Please calculate the average age of people who are in both dataframes """, # prompt that we send the agent
         "data": """data_1 = {'Name': ['Alice', 'Bob', 'Charlie'],'Age': [25, 30, 22],'City': ['New York', 'San Francisco', 'Los Angeles']}\ndata_2 = {'Name': ['Alice', 'John', 'Charlie'],'Age': [25, 31, 22],'City': ['New York', 'San Francisco', 'Los Angeles']}""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data_1 = pd.DataFrame(data1)\n    data_2 = pd.DataFrame(data2)\n    merged_df = pd.merge(data_1, data_2, on='Name')\n    data = merged_df['Age_x'].mean()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data_1, data_2):\n    data_1 = pd.DataFrame(data_1)\n    data_2 = pd.DataFrame(data_2)\n    merged_df = pd.merge(data_1, data_2, on='Name')\n    data = merged_df['Age_x'].mean()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 10,
         "user_prompt": """data = { 'Timestamp': [ '2023-01-01 12:01:00', '2023-01-01 12:10:00', '2023-01-01 12:25:00', '2023-01-01 13:05:00', '2023-01-01 13:25:00', '2023-01-01 14:00:00', '2023-01-02 08:30:00', '2023-01-02 09:00:00', '2023-01-02 09:35:00' ], 'User': [1, 1, 1, 2, 2, 2, 3, 3, 3], 'Page': ['Home', 'Product', 'Checkout', 'Home', 'Product', 'Home', 'Home', 'Product', 'Checkout'] } Using the pandas DataFrame df provided, implement the following operation: Create a new column called 'Session_ID' that labels each row with a unique session identifier. Define a session as a series of consecutive interactions by the same user with no gap greater than 30 minutes between interactions. Ensure that each session has a unique identifier. Make sure to show the hole code """, # prompt that we send the agent
-        "data": """data = pd.DataFrame={ 'Timestamp': [ '2023-01-01 12:01:00', '2023-01-01 12:10:00', '2023-01-01 12:25:00', '2023-01-01 13:05:00', '2023-01-01 13:25:00', '2023-01-01 14:00:00', '2023-01-02 08:30:00', '2023-01-02 09:00:00', '2023-01-02 09:35:00' ], 'User': [1, 1, 1, 2, 2, 2, 3, 3, 3], 'Page': ['Home', 'Product', 'Checkout', 'Home', 'Product', 'Home', 'Home', 'Product', 'Checkout'] }""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data['Timestamp'] = pd.to_datetime(data['Timestamp'])\n    data = data.sort_values(by=['User', 'Timestamp'])\n    data['TimeDiff'] = data.groupby('User')['Timestamp'].diff()\n    data['Session_ID'] = (data['TimeDiff'] > pd.Timedelta(minutes=30)).cumsum()\n    data = data.drop('TimeDiff', axis=1)\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "data": """data = pd.DataFrame({'Timestamp': ['2023-01-01 12:01:00', '2023-01-01 12:10:00', '2023-01-01 12:25:00', '2023-01-01 13:05:00','2023-01-01 13:25:00', '2023-01-01 14:00:00', '2023-01-02 08:30:00', '2023-01-02 09:00:00','2023-01-02 09:35:00'],'User': [1, 1, 1, 2, 2, 2, 3, 3, 3],'Page': ['Home', 'Product', 'Checkout', 'Home', 'Product', 'Home', 'Home', 'Product', 'Checkout']})""", # the data needed should always be named 'data'
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data['Timestamp'] = pd.to_datetime(data['Timestamp'])\n   data = data.sort_values(by=['User', 'Timestamp'])\n    data['TimeDiff'] = data.groupby('User')['Timestamp'].diff()\n     data['Session_ID'] = (data['TimeDiff'] > pd.Timedelta(minutes=30)).cumsum()\n    data = data.drop('TimeDiff', axis=1)\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 11,
@@ -82,20 +82,20 @@ TEST_CASES = [
     {
         "id": 13,
         "user_prompt": """Please take following dataframe (your argument) and group it for column A. Make sure to exclude the last value of each group.["g", "g0"], ["g", "g1"], ["g", "g2"], ["g", "g3"],["h", "h0"], ["h", "h1"]], columns=["A", "B"] """, # prompt that we send the agent
-        "data": """data = pd.DataFrame=["g", "g0"], ["g", "g1"], ["g", "g2"], ["g", "g3"],["h", "h0"], ["h", "h1"]], columns=["A", "B"]""", # the data needed should always be named 'data'
+        "data": """data = pd.DataFrame([["g", "g0"], ["g", "g1"], ["g", "g2"], ["g", "g3"], ["h", "h0"], ["h", "h1"]],columns=["A", "B"])""", # the data needed should always be named 'data'
         "correct_function": """import pandas as pd\ndef correct_function(data):\n    data.groupby("A").head(-1)\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 14,
         "user_prompt": """TPlease remove follwinf suffix “_str” from following Series (["foo_str","_strhead" , "text_str_text" , "bar_str", "no_suffix"]) """, # prompt that we send the agent
         "data": """data = pd.Series(["foo_str","_strhead" , "text_str_text" , "bar_str", "no_suffix"])""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data.str.removesuffix("_str")\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data.str.removesuffix("_str")\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 15,
         "user_prompt": """I have 2 Dataframes. which are you arguments The first one: pd.DataFrame({'key': ['K0', 'K1', 'K1', 'K3', 'K0', 'K1'],  'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']})And the second one: pd.DataFrame({'key': ['K0', 'K1', 'K2'], 'B': ['B0', 'B1', 'B2']})How do i join the second one on the first one using the key. And making sure it is a m:1 relation """, # prompt that we send the agent
-        "data": """data_1 = pd.DataFrame({'key': ['K0', 'K1', 'K1', 'K3', 'K0', 'K1'],  'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']})\ndata_2 = pd.DataFrame = ({'key': ['K0', 'K1', 'K2'], 'B': ['B0', 'B1', 'B2']})""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data_1.join(data_2.set_index('key'), on='key', validate='m:1')\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "data": """data_1 = pd.DataFrame({'key': ['K0', 'K1', 'K1', 'K3', 'K0', 'K1'],  'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']})\ndata_2 = pd.DataFrame({'key': ['K0', 'K1', 'K2'], 'B': ['B0', 'B1', 'B2']})""", # the data needed should always be named 'data'
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data_1.join(data_2.set_index('key'), on='key', validate='m:1')\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 16,
@@ -119,9 +119,10 @@ TEST_CASES = [
         "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = pd.Index([10, 20, 30, 40, 50])\n    data.diff()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
+            #Note Pyarrow needs to be installed pip install pyarrow
         "id": 19,
         "user_prompt": """df = pd.DataFrame({"a": [1, 1, 2, 1], "b": [np.nan, 2.0, 3.0, 4.0]}, dtype="Int64") This is my Dataframe. Please convert the Int64 to Int64[pyarrow] and use df.sum() at the end""", # prompt that we send the agent
         "data": """data = pd.DataFrame({"a": [1, 1, 2, 1], "b": [np.nan, 2.0, 3.0, 4.0]}, dtype="Int64")""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data= data.astype"int64[pyarrow]")\n    data.sum()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """\nimport pandas as pd\nimport pyarrow as pa\ndef correct_function(data):\n    data = data.astype"int64[pyarrow]")\n    data.sum()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     }
 ]
