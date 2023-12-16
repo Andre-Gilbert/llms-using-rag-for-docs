2ba6d1503b3fc69ec1c9a50dd8390dd0302775eb
Andre Gilbert <andre.gilbert.77110@gmail.com>
2023-12-16 11:19:02 +0100

    added config generator

---
+++ src/evaluate.py
@@ -0,0 +1,52 @@
+"""Tools used for evaluating AI agents."""
+from itertools import product
+from typing import Generator
+
+from pydantic import BaseModel
+
+from agent import AIAgent
+from rag import FAISS, CoALA, DistanceMetric
+
+
+class RAG(BaseModel):
+    retrievers: list[FAISS | CoALA | None]
+    distance_metrics: list[DistanceMetric]
+    normalize_L2: list[bool]
+    num_search_results: list[int]
+    text_chunk_size: list[int]
+    use_weighted_average_of_text_chunks: list[bool]
+
+
+class ConfigGrid(BaseModel):
+    agents: list[AIAgent]
+    rag: RAG
+
+
+def get_config_from_grid(config_grid: ConfigGrid) -> Generator[tuple, None, None]:
+    """Gets a config from the config grid using the cartesian product."""
+    for (
+        agent,
+        retriever,
+        distance_metric,
+        normalize_L2,
+        num_search_result,
+        text_chunk_size,
+        use_weighted_average_of_text_chunk,
+    ) in product(
+        config_grid.agents,
+        config_grid.rag.retrievers,
+        config_grid.rag.distance_metrics,
+        config_grid.rag.normalize_L2,
+        config_grid.rag.num_search_results,
+        config_grid.rag.text_chunk_size,
+        config_grid.rag.use_weighted_average_of_text_chunks,
+    ):
+        yield (
+            agent,
+            retriever,
+            distance_metric,
+            normalize_L2,
+            num_search_result,
+            text_chunk_size,
+            use_weighted_average_of_text_chunk,
+        )
