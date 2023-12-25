fbff97a0d559eea967ec198b53b73d585ca176f4
Andre Gilbert <andre.gilbert.77110@gmail.com>
2023-12-25 20:57:04 +0100

    added identifier

--- llms/evaluation/code.py
+++ llms/evaluation/code.py
@@ -1,8 +1,9 @@
 """Tools used for evaluating AI agents."""
 from enum import Enum
 from typing import Generator
+from uuid import uuid4
 
-from pydantic import BaseModel
+from pydantic import UUID4, BaseModel, Field
 from tqdm import tqdm
 from tqdm.contrib.itertools import product
 
@@ -11,6 +12,7 @@ from llms.rag.faiss import FAISS, DistanceMetric
 
 
 class CodeTestCase(BaseModel):
+    identifier: UUID4 = Field(default_factory=uuid4)
     prompt: str
     data: str
     correct_function: str
