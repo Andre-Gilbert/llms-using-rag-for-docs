979d8d84306dc83054058d22fabf84ede4fb24e2
Marc <marc.gruen@freenet.de>
2023-12-26 14:29:12 +0100

    rewritten correct_functions for test cases

--- tests/pandas.py
+++ tests/pandas.py
@@ -5,7 +5,7 @@ TEST_CASES = [
         "id": 0,
         "user_prompt": """How can I convert this dataframe: df = pd.DataFrame({"col1_a": [1, 0, 1], "col1_b": [0, 1, 0], "col2_a": [0, 1, 0], "col2_b": [1, 0, 0], "col2_c": [0, 0, 1]}) into a categorical dataframe?""", # prompt that we send the agent
         "data": """data = pd.DataFrame({"col1_a": [1, 0, 1], "col1_b": [0, 1, 0], "col2_a": [0, 1, 0], "col2_b": [1, 0, 0], "col2_c": [0, 0, 1]})""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data=pd.from_dummies(data, sep="_")\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    result = pd.from_dummies(data, sep="_")\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 1,
@@ -17,19 +17,19 @@ TEST_CASES = [
        "id": 2,
        "user_prompt": """({'animal': ['alligator', 'bee', 'falcon', 'lion','monkey', 'parrot', 'shark', 'whale', 'zebra']}) This is my dataframe. Please display all but the last 3 rows of the dataframe.""", # prompt that we send the agent
        "data": """data = pd.DataFrame({'animal': ['alligator', 'bee', 'falcon', 'lion','monkey', 'parrot', 'shark', 'whale', 'zebra']})""", # the data needed should always be named 'data'
-       "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data.iloc[:-3, :]\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+       "correct_function": """import pandas as pd\ndef correct_function(data):\n    result = data.iloc[:-3, :]\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 3,
         "user_prompt": """ts = pd.Timestamp('2017-01-01 09:10:11') This is your argument. Please add 2 Months to that timestamp.""", # prompt that we send the agent
         "data": """data = pd.Timestamp('2017-01-01 09:10:11')""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data + pd.DateOffset(months=2)\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    result = data + pd.DateOffset(months=2)\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 4,
         "user_prompt": """ser = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd']). Please calculate the expending sum of that series. Make sure to display each row.""", # prompt that we send the agent
         "data": """data = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data.expanding().sum()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    result = data.expanding().sum()\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 5,
@@ -41,25 +41,25 @@ TEST_CASES = [
         "id": 6,
         "user_prompt": """a = pd.Series([1, 1, 1, np.nan], index=['a', 'b', 'c', 'd']) b = pd.Series([1, np.nan, 1, np.nan], index=['a', 'b', 'd', 'e'])  Please take a and b as your arguments and divide a by b. Please also use the fill value 0.""", # prompt that we send the agent
         "data": """import numpy as np\ndata_1 = pd.Series([1, 1, 1, np.nan], index=['a', 'b', 'c', 'd'])\ndata_2 = pd.Series([1, np.nan, 1, np.nan], index=['a', 'b', 'd', 'e'])""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\nimport numpy as np\ndef correct_function(data_1, data_2):\n    data = data_1.div(data_2, fill_value=0)\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\nimport numpy as np\ndef correct_function(data_1, data_2):\n    result = data_1.div(data_2, fill_value=0)\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 7,
         "user_prompt": """data = {('level_1', 'c', 'a'): [3, 7, 11],('level_1', 'd', 'b'): [4, 8, 12],('level_2', 'e', 'a'): [5, 9, None],('level_2', 'f', 'b'): [6, 10, None],}Please drop column a.""", # prompt that we send the agent
         "data": """data = pd.DataFrame({('level_1', 'c', 'a'): [3, 7, 11],('level_1', 'd', 'b'): [4, 8, 12],('level_2', 'e', 'a'): [5, 9, None],('level_2', 'f', 'b'): [6, 10, None],})""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data.droplevel(2, axis=1)\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    result = data.droplevel(2, axis=1)\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 8,
         "user_prompt": """Please take following Series and order it ascending while making sure NAN values are at the beginning s = pd.Series([np.nan, 1, 3, 10, 5, np.nan]) """, # prompt that we send the agent
         "data": """import numpy as np\ndata = pd.Series([np.nan, 1, 3, 10, 5, np.nan])""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data.sort_values(na_position='first')\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    result = data.sort_values(na_position='first')\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 9,
         "user_prompt": """data1 = {'Name': ['Alice', 'Bob', 'Charlie'],'Age': [25, 30, 22],'City': ['New York', 'San Francisco', 'Los Angeles']} data2= {'Name': ['Alice', 'John', 'Charlie'],'Age': [25, 31, 22],'City': ['New York', 'San Francisco', 'Los Angeles']}Please calculate the average age of the people who appear in both dataframes.""", # prompt that we send the agent
         "data": """data_1 = {'Name': ['Alice', 'Bob', 'Charlie'],'Age': [25, 30, 22],'City': ['New York', 'San Francisco', 'Los Angeles']}\ndata_2 = {'Name': ['Alice', 'John', 'Charlie'],'Age': [25, 31, 22],'City': ['New York', 'San Francisco', 'Los Angeles']}""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data_1, data_2):\n    data_1 = pd.DataFrame(data_1)\n    data_2 = pd.DataFrame(data_2)\n    merged_df = pd.merge(data_1, data_2, on='Name')\n    data = merged_df['Age_x'].mean()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data_1, data_2):\n    df_1 = pd.DataFrame(data_1)\n    df_2 = pd.DataFrame(data_2)\n    merged_df = pd.merge(df_1, df_2, on='Name')\n    result = merged_df['Age_x'].mean()\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 10,
@@ -71,31 +71,31 @@ TEST_CASES = [
         "id": 11,
         "user_prompt": """Please return the rolling rank(3) of this Series [1, 4, 2, 3, 5, 3]. Make sure to code your solution using the pandas lib.""", # prompt that we send the agent
         "data": """data = pd.Series([1, 4, 2, 3, 5, 3])""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data.rolling(3).rank()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    result = data.rolling(3).rank()\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 12,
         "user_prompt": """ Please create a dictionary using the following Dataframe. This dataframe is your argument. Make sure to order it tight. pd.DataFrame([[1, 3], [2, 4]],index=pd.MultiIndex.from_tuples([("a", "b"), ("a", "c")],names=["n1", "n2"]),columns=pd.MultiIndex.from_tuples([("x", 1), ("y", 2)], names=["z1", "z2"]),)""", # prompt that we send the agent
         "data": """data = pd.DataFrame.from_records([[1, 3], [2, 4]],index=pd.MultiIndex.from_tuples([("a", "b"), ("a", "c")],names=["n1", "n2"]),columns=pd.MultiIndex.from_tuples([("x", 1), ("y", 2)], names=["z1", "z2"]),)""", # the data needed should always be named 'data'"correct_function": """\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
-        "correct_function":"""import pandas as pd\ndef correct_function(data):\n    data.to_dict(orient='tight')\n    return data"""
+        "correct_function":"""import pandas as pd\ndef correct_function(data):\n    result = data.to_dict(orient='tight')\n    return result"""
     },
     {
         "id": 13,
         "user_prompt": """Please take following dataframe (your argument) and group it for column A. Make sure to exclude the last value of each group. This is your argument data = pd.DataFrame(["g", "g0"], ["g", "g1"], ["g", "g2"], ["g", "g3"],["h", "h0"], ["h", "h1"]], columns=["A", "B"]).""", # prompt that we send the agent
         "data": """data = pd.DataFrame([["g", "g0"], ["g", "g1"], ["g", "g2"], ["g", "g3"], ["h", "h0"], ["h", "h1"]],columns=["A", "B"])""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data.groupby("A").head(-1)\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    result = data.groupby("A").head(-1)\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 14,
         "user_prompt": """Please remove the following suffix “_str” from following Series (["foo_str","_strhead" , "text_str_text" , "bar_str", "no_suffix"]) """, # prompt that we send the agent
         "data": """data = pd.Series(["foo_str","_strhead" , "text_str_text" , "bar_str", "no_suffix"])""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data.str.removesuffix("_str")\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    result = data.str.removesuffix("_str")\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 15,
         "user_prompt": """I have 2 Dataframes which are you arguments. The first one: pd.DataFrame({'key': ['K0', 'K1', 'K1', 'K3', 'K0', 'K1'],  'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']}) And the second one: pd.DataFrame({'key': ['K0', 'K1', 'K2'], 'B': ['B0', 'B1', 'B2']})How do I join the second one on the first one using the key and making sure it is a m:1 relation?""", # prompt that we send the agent
         "data": """data_1 = pd.DataFrame({'key': ['K0', 'K1', 'K1', 'K3', 'K0', 'K1'],  'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']})\ndata_2 = pd.DataFrame({'key': ['K0', 'K1', 'K2'], 'B': ['B0', 'B1', 'B2']})""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data_1, data_2):\n    data = data_1.join(data_2.set_index('key'), on='key', validate='m:1')\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data_1, data_2):\n    result = data_1.join(data_2.set_index('key'), on='key', validate='m:1')\n    return result""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
          "id": 16,
@@ -107,13 +107,13 @@ TEST_CASES = [
         "id": 17,
         "user_prompt": """What are the value counts of this function pd.Series(['quetzal', 'quetzal', 'elk'], name='animal')?""", # prompt that we send the agent
         "data": """data = pd.Series(['quetzal', 'quetzal', 'elk'], name='animal')""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data=pd.Series(['quetzal', 'quetzal', 'elk'], name='animal').value_counts()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    result = data.value_counts()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 18,
         "user_prompt": """Please compute the difference between these consecutive values as an index object: pd.Index([10, 20, 30, 40, 50]).""", # prompt that we send the agent
         "data": """data = pd.Index([10, 20, 30, 40, 50])""", # the data needed should always be named 'data'
-        "correct_function": """import pandas as pd\ndef correct_function(data):\n    data = data.diff()\n    return data""", # this is a response function that takes the parameter 'data' and does the correct thing with it
+        "correct_function": """import pandas as pd\ndef correct_function(data):\n    sum = data.diff()\n    return sum""", # this is a response function that takes the parameter 'data' and does the correct thing with it
     },
     {
         "id": 19,
